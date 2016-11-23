b8117d0afd5ce0143f2b7d1fd191539b075f8b90
Ryan MacGillivray <rmacg89@gmail.com>
2016-11-23 16:38:38 +0000

    Use lets in test

--- spec/models/spree/promotion_spec.rb
+++ spec/models/spree/promotion_spec.rb
@@ -1,18 +1,15 @@
 require 'spec_helper'
 
 describe Spree::Promotion do
-  before(:each) do
-    @store = FactoryGirl.create(:store)
-    store2 = FactoryGirl.create(:store)
-
-    @promotion = FactoryGirl.create(:promotion, store: @store)
-    @promotion2 = FactoryGirl.create(:promotion, store: store2)
-  end
+  let(:store) { FactoryGirl.create(:store) }
+  let(:store2) { FactoryGirl.create(:store) }
+  let(:promotion) { FactoryGirl.create(:promotion, store: store) }
+  let(:promotion2) { FactoryGirl.create(:promotion, store: store2) }
 
   it 'should correctly find promotions by store' do
-    promotions_by_store = Spree::Promotion.by_store(@store)
+    promotions_by_store = Spree::Promotion.by_store(store)
 
-    expect(promotions_by_store).to include(@promotion)
-    expect(promotions_by_store).to_not include(@promotion2)
+    expect(promotions_by_store).to include(promotion)
+    expect(promotions_by_store).to_not include(promotion2)
   end
 end
